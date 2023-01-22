72e4e4b591ef596e53531050107017185d1601ce
cbl980226 <binglinchen98@gmail.com>
2023-01-22 17:18:42 +0800

    fix(ci): test job selenium error

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,7 @@
 // import { Driver, ServiceBuilder, Options } from 'selenium-webdriver/ie';
 // import { driverLocation } from '../tools/selenium-manager';
 import { Browser, Builder, By, WebDriver } from 'selenium-webdriver';
+import chrome from 'selenium-webdriver/chrome';
 import expect from 'expect';
 import { rollup } from './index';
 
@@ -10,9 +11,15 @@ describe('Simple tests', () => {
   before(async () => {
     // const service = new ServiceBuilder(driverLocation('ie')).build();
     // const options = new Options();
-
     // driver = Driver.createSession(options, service);
-    driver = await new Builder().forBrowser(Browser.CHROME).build();
+
+    const options = new chrome.Options();
+    options.addArguments('--no-sandbox');
+    options.addArguments('--disable-dev-shm-usage');
+    options.addArguments('--headless');
+    options.addArguments('--disable-gpu');
+    options.addArguments('blink-settings=imagesEnabled=false');
+    driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build();
   });
 
   after(async () => await driver.quit());
